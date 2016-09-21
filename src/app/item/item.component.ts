ede7ded48f068ed59e69df8069b0003a259fbd24
Sun <hyunsang.sun@samsung.com>
2016-09-21 10:05:47 +0900

    pipes and async test

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'my-item',
@@ -7,10 +9,19 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
-  name:string;
+  name: string;
+  fetchName: Promise<string>;
+  obName: Observable<string>;
 
   constructor() {
     this.name = 'Sun';
+    this.fetchName = new Promise((resolve, reject) => {
+      setTimeout(() => resolve('Sun'), 1000);
+    });
+    this.obName = Observable.create((observer:Observer<string>) => {
+      observer.next("Sun");
+      setInterval(() => { observer.next("Kim"); }, 1000);
+    });
   }
 
   onClick() {
--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -3,12 +3,18 @@ import { CommonModule }  from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ItemComponent } from './item.component';
 
+import { UpperPipe } from './upper.pipe';
+
 @NgModule({
     imports: [
         CommonModule,
         FormsModule
     ],
     declarations: [
+        ItemComponent,
+        UpperPipe
+    ],
+    exports : [
         ItemComponent
     ]
 })
